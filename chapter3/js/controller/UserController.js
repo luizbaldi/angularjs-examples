cdb2ea3d7a7bd1b127931ec36e56c18f1a7ec005
Luiz Baldi <baldilp@gmail.com>
2016-11-17 19:09:43 -0200

    adding directive examples

---
+++ chapter3/js/controller/UserController.js
@@ -0,0 +1,7 @@
+app.controller('UserController', function($scope) {
+	$scope.user = {
+		name: "Luiz Baldi",
+		account: "23840",
+		agency: "41321-22"
+	};
+});
---
+++ chapter3/js/directive/SimpleDirective.js
@@ -0,0 +1,6 @@
+app.directive('simpleDirective', function() {
+	return {
+		restrict: 'A',
+		templateUrl: 'js/directive/template/simple-directive.html'
+	}
+});
